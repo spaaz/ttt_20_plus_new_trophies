16d5ddb1fdd65c7af5c0d05248643812ddafdfe1
The Stig <TheStig294@users.noreply.github.com>
2023-02-06 14:35:54 +1030

    Fixed trophy erroring when killing an innocent as a traitor when the traitor joined the game late

--- lua/ttt_trophies/trophies/terroristslayer.lua
+++ lua/ttt_trophies/trophies/terroristslayer.lua
@@ -6,20 +6,25 @@ TROPHY.rarity = 2
 
 function TROPHY:Trigger()
     self.roleMessage = ROLE_TRAITOR
-	self:AddHook( "TTTBeginRound", function()
-		for _, ply in ipairs(player.GetAll()) do
-			ply.terrslay = 0
-        end
-	end)
 
-    self:AddHook("DoPlayerDeath", function(tgt,att,dmginf)
-		if IsPlayer(att) and (att:IsActiveTraitor() or (CR_VERSION and att:IsTraitorTeam())) and ((not CR_VERSION and not tgt:IsActiveTraitor()) or (CR_VERSION and (tgt:IsInnocentTeam() or tgt:IsMonsterTeam() or tgt:IsIndependentTeam() or ((tgt:GetRole() == ROLE_CLOWN) and tgt:GetNWBool("KillerClownActive", false))))) then	
-			if att.terrslay < 2 then
-				att.terrslay = att.terrslay + 1
-			else
-				self:Earn(att)
-			end
-		end
+    self:AddHook("TTTBeginRound", function()
+        for _, ply in ipairs(player.GetAll()) do
+            ply.terrslay = 0
+        end
+    end)
+
+    self:AddHook("DoPlayerDeath", function(tgt, att, dmginf)
+        if IsPlayer(att) and (att:IsActiveTraitor() or (CR_VERSION and att:IsTraitorTeam())) and ((not CR_VERSION and not tgt:IsActiveTraitor()) or (CR_VERSION and (tgt:IsInnocentTeam() or tgt:IsMonsterTeam() or tgt:IsIndependentTeam() or ((tgt:GetRole() == ROLE_CLOWN) and tgt:GetNWBool("KillerClownActive", false))))) then
+            if not att.terrslay then
+                att.terrslay = 0
+            end
+
+            if att.terrslay < 2 then
+                att.terrslay = att.terrslay + 1
+            else
+                self:Earn(att)
+            end
+        end
     end)
 end
 
