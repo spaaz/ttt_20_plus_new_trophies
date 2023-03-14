c960e8a51ce3e1516601c1fc2ca144e226fda270
The Stig <TheStig294@users.noreply.github.com>
2023-03-14 21:14:31 +1030

    Added TROPHY.forceDesc to forcibly show the descriptions of trophies that are too hard to discover while hidden

--- lua/ttt_trophies/trophies/ishouldntbealive.lua
+++ lua/ttt_trophies/trophies/ishouldntbealive.lua
@@ -3,28 +3,29 @@ TROPHY.id = "ishouldntbealive"
 TROPHY.title = "I shouldn\'t be alive"
 TROPHY.desc = "Survive a shark trap with an uno reverse"
 TROPHY.rarity = 3
+TROPHY.forceDesc = true
 
 function TROPHY:Trigger()
     self.roleMessage = ROLE_DETECTIVE
 
-    self:AddHook("EntityTakeDamage", function(tgt,dmginf)
-		if IsPlayer(tgt) and tgt:GetNWBool("HasUNOReverse") then
-			local wep = dmginf:GetInflictor()
-			if IsValid(wep) and wep:GetClass() == "ttt_shark_trap" then
-				self:Earn(tgt)
-			end
-		end
+    self:AddHook("EntityTakeDamage", function(tgt, dmginf)
+        if IsPlayer(tgt) and tgt:GetNWBool("HasUNOReverse") then
+            local wep = dmginf:GetInflictor()
+
+            if IsValid(wep) and wep:GetClass() == "ttt_shark_trap" then
+                self:Earn(tgt)
+            end
+        end
     end)
 end
 
 function TROPHY:Condition()
-	for _, addon in ipairs( engine.GetAddons()) do
-		if addon.mounted then
-			if addon.wsid == "2329721936" then
-				return scripted_ents.Get("ttt_shark_trap") ~= nil
-			end
-		end
-	end
+    for _, addon in ipairs(engine.GetAddons()) do
+        if addon.mounted then
+            if addon.wsid == "2329721936" then return scripted_ents.Get("ttt_shark_trap") ~= nil end
+        end
+    end
+
     return false
 end
 
--- lua/ttt_trophies/trophies/wombocombo.lua
+++ lua/ttt_trophies/trophies/wombocombo.lua
@@ -3,25 +3,25 @@ TROPHY.id = "wombocombo"
 TROPHY.title = "Wombo combo"
 TROPHY.desc = "Survive your own rail gun explosion with an immortality potion"
 TROPHY.rarity = 3
+TROPHY.forceDesc = true
 
 function TROPHY:Trigger()
     self.roleMessage = ROLE_INNOCENT
 
-    self:AddHook("EntityTakeDamage", function(tgt,dmginf)
-		if IsPlayer(tgt) and tgt:HasGodMode() then
-			local att = dmginf:GetAttacker()
-			local wep = tgt:GetActiveWeapon()
-			if IsPlayer(att) and (att == tgt) and IsValid(wep) and wep:GetClass() == "weapon_rp_railgun" then
-				self:Earn(tgt)
-			end
-		end
+    self:AddHook("EntityTakeDamage", function(tgt, dmginf)
+        if IsPlayer(tgt) and tgt:HasGodMode() then
+            local att = dmginf:GetAttacker()
+            local wep = tgt:GetActiveWeapon()
+
+            if IsPlayer(att) and (att == tgt) and IsValid(wep) and wep:GetClass() == "weapon_rp_railgun" then
+                self:Earn(tgt)
+            end
+        end
     end)
 end
 
 function TROPHY:Condition()
-
-	return (weapons.Get("weapon_ttt_mc_immortpotion") ~= nil) and (weapons.Get("weapon_rp_railgun") ~= nil) and GetConVar("ttt_mc_immort_enabled") and GetConVar("ttt_mc_immort_enabled"):GetBool() and GetConVar("ttt_mc_immort_force_active") and not GetConVar("ttt_mc_immort_force_active"):GetBool()
-
+    return (weapons.Get("weapon_ttt_mc_immortpotion") ~= nil) and (weapons.Get("weapon_rp_railgun") ~= nil) and GetConVar("ttt_mc_immort_enabled") and GetConVar("ttt_mc_immort_enabled"):GetBool() and GetConVar("ttt_mc_immort_force_active") and not GetConVar("ttt_mc_immort_force_active"):GetBool()
 end
 
 RegisterTTTTrophy(TROPHY)
