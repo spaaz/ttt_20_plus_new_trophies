83e24874d5632e4e8e6460101a11882d92248401
The Stig <TheStig294@users.noreply.github.com>
2023-05-07 16:27:07 +0930

    Re-wrote uno reverse a shark trap trophy to get around the weird null inflictor bug the shark trap has Also simplified its condition function

--- lua/ttt_trophies/trophies/ishouldntbealive.lua
+++ lua/ttt_trophies/trophies/ishouldntbealive.lua
@@ -7,26 +7,38 @@ TROPHY.forceDesc = true
 
 function TROPHY:Trigger()
     self.roleMessage = ROLE_DETECTIVE
+    -- Override shark trap to trigger trophy as shark trap has a weird bug with passing its inflictor
+    -- and we will have to hack the shark trap like this to fix that anyway
+    local ENT = scripted_ents.GetStored("ttt_shark_trap").t
+    local oldTouch = ENT.Touch
 
-    self:AddHook("EntityTakeDamage", function(tgt, dmginf)
-        if IsPlayer(tgt) and tgt:GetNWBool("HasUNOReverse") then
-            local wep = dmginf:GetInflictor()
+    function ENT:Touch(toucher)
+        if not IsValid(toucher) or not toucher:IsPlayer() then return end
 
-            if IsValid(wep) and wep:GetClass() == "ttt_shark_trap" then
-                self:Earn(tgt)
+        -- 1.6 seconds is how long the shark trap waits to kill the player after they step on it
+        timer.Simple(1.6, function()
+            if not IsValid(toucher) then return end
+            local wep = toucher:GetActiveWeapon()
+
+            if IsValid(wep) and wep:GetClass() == "weapon_unoreverse" then
+                -- Don't earn the trophy right away in case the uno reverse didn't work for whatever reason and the player is dead
+                timer.Simple(0.1, function()
+                    if IsValid(toucher) and toucher:Alive() and not toucher:IsSpec() then
+                        TROPHY:Earn(toucher)
+                    end
+                end)
             end
-        end
-    end)
+        end)
+
+        return oldTouch(self, toucher)
+    end
 end
 
 function TROPHY:Condition()
-    for _, addon in ipairs(engine.GetAddons()) do
-        if addon.mounted then
-            if addon.wsid == "2329721936" then return scripted_ents.Get("ttt_shark_trap") ~= nil end
-        end
-    end
+    local unoSWEP = weapons.Get("weapon_unoreverse")
+    -- Uno reverse can be installed, but only is a functioning TTT weapon if it has a weapon kind
 
-    return false
+    return weapons.Get("weapon_shark_trap") ~= nil and unoSWEP ~= nil and unoSWEP.Kind
 end
 
 RegisterTTTTrophy(TROPHY)
